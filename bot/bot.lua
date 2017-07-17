f980b25651c75b03170aa7398747f9e78a5f35b0
aryangh <aryangh@users.noreply.github.com>
2017-07-17 22:10:21 +0430

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -1,5 +1,3 @@
--- #Beyond Reborn Robot
--- #@BeyondTeam
 
 tdcli = dofile('./tg/tdcli.lua')
 serpent = (loadfile "./libs/serpent.lua")()
@@ -90,28 +88,24 @@ function create_config( )
 		"fun",
 
 	},
-    sudo_users = {111334847, 157059515, sudo_id},
+    sudo_users = {115740444, sudo_id},
     admins = {},
     disabled_channels = {},
     moderation = {data = './data/moderation.json'},
-    info_text = [[》Beyond Reborn v6.0
+    info_text = [[》PowerShield 
 An advanced administration bot based on https://valtman.name/telegram-cli
 
-》https://github.com/BeyondTeam/BDReborn 
+》In The Name Of Allah
 
 》Admins :
-》@SoLiD ➣ Founder & Developer《
-》@Makan ➣ Developer《
-》@ToOfan ➣ Developer《
+》@DarkDeveloper ➣ Founder & Developer《
 
 》Special thanks to :
-》Beyond Team Members
+PowerShield Team Members
 
 》Our channel :
-》@BeyondTeam《
+》@PowerShield_Team《
 
-》Our website :
-》http://Beyond-Dev.iR
 ]],
   }
 	serialize_to_file(config, './data/config.lua')
